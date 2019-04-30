ba1b314c6efb8d6818d54cdea3d6b03c856d0455
Henrik René Fennen <h.fennen@tu-bs.de>
2019-04-30 02:15:52 +0200

    dirty hack for spawner direction

--- scenes/EnemySpawner.gd
+++ scenes/EnemySpawner.gd
@@ -2,7 +2,10 @@ extends Node2D
 
 export (Resource) var enemy_scene : Resource
 
+export var direction : Vector2
+
 func _ready():#spawn_enemy():
 	var node = enemy_scene.instance()
 	node.set_global_position(get_global_position())
+	node.direction = direction
 	add_child(node)
