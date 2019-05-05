66e01e5316513a601c4b0769c46fecf481772f45
Henrik René Fennen <h.fennen@tu-bs.de>
2019-05-05 17:55:49 +0200

    redid bug fix of try again button

--- scenes/gameOver.gd
+++ scenes/gameOver.gd
@@ -9,8 +9,8 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_Button2_pressed():
-	get_tree().change_scene("res://scenes/main.tscn")
-
+	#get_tree().change_scene("res://scenes/main.tscn")
+	global.GameStart()
 
 func _on_Button_pressed():
 	get_tree().quit()
